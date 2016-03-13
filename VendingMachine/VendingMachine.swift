6d83839647f0db2ed79117b7c39d32f9e3cc770f
Wolfgang Ettlich <Wolfgang.Ettlich@gmx.net>
2016-03-13 18:27:27 +0800

    modelling a vending machine interface

--- VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine.swift
@@ -8,4 +8,58 @@
 
 import Foundation
 
-let userName = "bla"
+
+// Protocols
+
+protocol VendingMachineType {
+    var selection: [VendingSelection] {get}
+    var inventory: [VendingSelection: ItemType] {get set}
+    var amountDeposited: Double {get set}
+
+    init(inventory: [VendingSelection: ItemType])
+    func vend(selection: VendingSelection, quantity: Double) throws
+    func deposit(amount: Double)
+}
+
+protocol ItemType {
+    var price: Double {get}
+    var quantity: Double {get set}
+}
+
+enum VendingSelection {
+    case Soda
+    case DietSoda
+    case Chips
+    case Cookie
+    case Sandwich
+    case Wrap
+    case CandyBar
+    case PopTart
+    case Water
+    case FruitJuice
+    case SportsDrink
+    case Gum
+}
+
+struct VendingItem: ItemType {
+    let price: Double
+    var quantity: Double
+    
+}
+
+class VendingMachine: VendingMachineType {
+    var selection: [VendingSelection] = [.Soda, .DietSoda, .Chips, .Cookie, .Sandwich, .Wrap, .CandyBar, .PopTart, .Water, .FruitJuice, .SportsDrink, .Gum]
+    var inventory: [VendingSelection: ItemType]
+    var amountDeposited: Double = 10.0
+    required init(inventory: [VendingSelection: ItemType]){
+        self.inventory = inventory
+    }
+    
+    func vend(selection: VendingSelection, quantity: Double) throws {
+        // add code
+    }
+    
+    func depost(amount: Double) {
+        // add code
+    }
+}
